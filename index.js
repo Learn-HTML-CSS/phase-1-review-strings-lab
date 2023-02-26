4997350736e60128d5e22d34833666ee18812311
Newton Ombese <newtonombese1@gmail.com>
2023-02-26 09:29:58 +0300

    :art:feat: Working with strings concatenation

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Newton Jillian"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+console.log(welcomeMessage)
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+let shortGreeting = `Welcome, ${currentUser[0]}!`
